7521455a70525493834b1463ee5571c17519b5c9
Chakwen Hong <swgrhck@gmail.com>
2018-05-29 22:14:51 +0800

    Add test script amd config for BrowserStack

---
+++ test/karma-cloud.conf.js
@@ -0,0 +1,28 @@
+/* eslint-env node */
+const pkg = require('../package')
+const browsers = require('./browsers')
+
+module.exports = function (config) {
+  config.set({
+    basePath: '..',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      'dist/bundle/scroll-monitor.umd.js',
+      'test/unit/*.js'
+    ],
+    reporters: ['dots', 'BrowserStack'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    singleRun: true,
+    concurrency: Infinity,
+    browserStack: {
+      username: process.env.BROWSERSTACK_USERNAME,
+      accessKey: process.env.BROWSERSTACK_KEY,
+      project: pkg.name
+    },
+    customLaunchers: browsers,
+    browsers: Object.keys(browsers)
+  })
+}
