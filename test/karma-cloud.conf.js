a6c4de22e81248c9d32a097216a08855b045f8bf
Chakwen Hong <swgrhck@gmail.com>
2018-06-04 22:46:05 +0800

    Add iphone 10 to browsers.json

--- test/karma-cloud.conf.js
+++ test/karma-cloud.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       'test/unit/*.js'
     ],
     reporters: ['dots', 'BrowserStack'],
+    hostname: '127.0.0.1',
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
